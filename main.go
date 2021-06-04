94285434669609f2d34decd0bb0ec7da1a4fe62b
gaoyangy <yangyangae86@gmail.com>
2021-06-05 02:47:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,9 +10,17 @@ const webContent = "Hello World!"
 
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/a", helloHandler)
+	http.HandleFunc("/b", helloHandler)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, webContent)
+}
+func helloHandler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, webContent)
+}
+func helloHandler2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, webContent)
 }
